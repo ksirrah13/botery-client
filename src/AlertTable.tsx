de3ab96bdfc508f02259a3b3e579021273ab41f4
Kyle Harris <kyle@learnwithjuni.com>
2023-04-05 23:34:09 -0700

    add sorting and styling

--- src/AlertTable.tsx
+++ src/AlertTable.tsx
@@ -11,6 +11,7 @@ export const AlertTable = ({
   refreshAlerts: () => void;
   showAllTimes: boolean;
 }) => {
+  const headerCellStyle = 'p-2 border border-solid bg-slate-200 font-semibold';
   return (
     <div>
       <h2>Alerts</h2>
@@ -23,31 +24,44 @@ export const AlertTable = ({
       >
         Refresh Alerts
       </Button>
-      <table>
-        <tr>
-          <td>User</td>
-          <td>Date</td>
-          <td>Start Time</td>
-          <td>End Time</td>
-          <td>Court Name</td>
-          <td>Status</td>
-        </tr>
-        {alerts
-          .filter((alert) => {
-            console.log({
-              date: alert.date,
-              millis: new Date(alert.date).getTime(),
-              now: Date.now(),
-            });
-            return showAllTimes || new Date(alert.date).getTime() > Date.now();
-          })
-          .map((alert) => (
-            <AlertRow
-              key={alert._id}
-              alert={alert}
-              refreshAlerts={refreshAlerts}
-            />
-          ))}
+      <table className="border-separate border border-solid">
+        <thead>
+          <tr>
+            <td className={headerCellStyle}>User</td>
+            <td className={headerCellStyle}>Date</td>
+            <td className={headerCellStyle}>Court Name</td>
+            <td className={headerCellStyle}>Start Time</td>
+            <td className={headerCellStyle}>End Time</td>
+            <td className={headerCellStyle}>Status</td>
+            <td className={headerCellStyle}>Actions</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {alerts
+            .filter((alert) => {
+              console.log({
+                date: alert.date,
+                millis: new Date(alert.date).getTime(),
+                now: Date.now(),
+              });
+              return (
+                showAllTimes || new Date(alert.date).getTime() > Date.now()
+              );
+            })
+            .sort(
+              (alertA, alertB) =>
+                new Date(alertA.date).getTime() -
+                new Date(alertB.date).getTime()
+            )
+            .map((alert) => (
+              <AlertRow
+                key={alert._id}
+                alert={alert}
+                refreshAlerts={refreshAlerts}
+              />
+            ))}
+        </tbody>
       </table>
     </div>
   );
@@ -60,23 +74,47 @@ const AlertRow = ({
   alert: Alert;
   refreshAlerts: () => void;
 }) => {
+  const cellStyle = 'border border-solid p-2';
+  const getAlertStyle = (status: string) => {
+    const statusStyle = status === 'new' ? 'bg-green-100' : 'bg-red-100';
+    return `${cellStyle} ${statusStyle}`;
+  };
+  const getTime = (dateInput: Date) => {
+    const date = new Date(dateInput);
+    return `${date.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: 'UTC',
+    })}`;
+  };
+  const getDate = (dateInput: Date) => {
+    const date = new Date(dateInput);
+    return `${date.toLocaleDateString('en-US', {
+      month: 'short',
+      day: '2-digit',
+      weekday: 'short',
+      timeZone: 'UTC',
+    })}`;
+  };
   return (
     <tr>
-      <td>{alert.userId}</td>
-      <td>{alert.date.toString()}</td>
-      <td>{alert.startTime.toString()}</td>
-      <td>{alert.endTime.toString()}</td>
-      <td>{getCourtName(alert.courtId)}</td>
-      <td>{alert.status}</td>
-      <Button
-        onClick={async (e) => {
-          e.preventDefault();
-          await deleteAlert(alert._id);
-          await refreshAlerts();
-        }}
-      >
-        Delete
-      </Button>
+      <td className={cellStyle}>{alert.userId}</td>
+      <td className={cellStyle}>{getDate(alert.date)}</td>
+      <td className={cellStyle}>{getCourtName(alert.courtId)}</td>
+      <td className={cellStyle}>{getTime(alert.startTime)}</td>
+      <td className={cellStyle}>{getTime(alert.endTime)}</td>
+      <td className={getAlertStyle(alert.status)}>{alert.status}</td>
+      <td className={cellStyle}>
+        <Button
+          onClick={async (e) => {
+            e.preventDefault();
+            await deleteAlert(alert._id);
+            await refreshAlerts();
+          }}
+        >
+          Delete
+        </Button>
+      </td>
     </tr>
   );
 };
